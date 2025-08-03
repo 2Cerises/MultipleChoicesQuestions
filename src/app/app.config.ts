0352a8cdaecec42107fe9aea0b01860ad3290b8d
HousseinDhayne <dhayne_houssein@hotmail.com>
2025-08-03 19:02:19 -0400

    remove formly

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideFormlyCore } from '@ngx-formly/core'
-import { withFormlyBootstrap } from '@ngx-formly/bootstrap';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideFormlyCore(withFormlyBootstrap())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
