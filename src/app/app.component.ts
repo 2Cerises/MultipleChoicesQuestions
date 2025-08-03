c3f7fb28510a15373e0dad28ab14ea28ebf3ce95
HousseinDhayne <dhayne_houssein@hotmail.com>
2025-08-03 19:51:56 -0400

    remove unused component import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { QuestionComponent } from "./question/question.component";
 import { MultiquestionsComponent } from "./multiquestions/multiquestions.component";
 @Component({
   selector: 'app-root',
-  imports: [QuestionComponent, MultiquestionsComponent],
+  imports: [MultiquestionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
