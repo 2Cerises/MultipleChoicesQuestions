f9bc07b5a2276c9f20629411d05a821726d2660a
HousseinDhayne <dhayne_houssein@hotmail.com>
2025-08-03 11:51:43 -0400

    add necessary imports to app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideFormlyCore } from '@ngx-formly/core'
+import { withFormlyBootstrap } from '@ngx-formly/bootstrap';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideFormlyCore(withFormlyBootstrap())]
 };
